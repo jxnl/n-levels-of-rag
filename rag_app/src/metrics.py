66c3ed1210aa21994e6ef3627eed8e779efbc66b
Ivan <ivanleomk@gmail.com>
2024-03-18 18:39:59 +0800

    Removed the prediction length printing

--- rag_app/src/metrics.py
+++ rag_app/src/metrics.py
@@ -3,12 +3,10 @@ import numpy as np
 
 
 def calculate_mrr(chunk_id, predictions):
-    print(len(predictions))
     return 0 if chunk_id not in predictions else 1 / (predictions.index(chunk_id) + 1)
 
 
 def calculate_ndcg(chunk_id, predictions):
-    print(len(predictions))
     y_pred = np.linspace(1, 0, len(predictions)).tolist()
     y_true = [0 if item != chunk_id else 1 for item in predictions]
 
